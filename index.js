898d6787aaf63f59604b24b3f195bb84c718e2ac
Renze Nicolai <renze@rnplus.nl>
2026-05-03 03:42:02 +0200

    Fix filtering by device for both projects and categories

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ import fs from 'node:fs/promises';
 import swagger_ui from 'swagger-ui-dist';
 
 const port = 8001;
-const repository_path = Path.join(Path.dirname(fileURLToPath(import.meta.url)), 'repository');
+let repository_path = Path.join(Path.dirname(fileURLToPath(import.meta.url)), 'repository');
 const data_path = '/repository';
 
+repository_path = (typeof process.argv[2] === "string") ? process.argv[2] : repository_path;
 const repository_version = Math.floor(new Date().getTime() / 1000);
 
 const app = new express();
@@ -60,8 +61,10 @@ class Repository {
         this.repository_path = repository_path;
         this.index = {
             categories: {},
+            categories_per_device: {},
         };
         this.projects = {};
+        this.projects_per_device = {};
         console.log('Repository path is ', this.repository_path);
         this.load();
     }
@@ -69,19 +72,21 @@ class Repository {
     async load() {
         await this.load_index();
         await this.load_projects();
+        await this.create_index_per_device();
     }
 
     async load_index() {
         this.index = {
-            categories: {}
+            categories: {},
+            categories_per_device: {},
         };
 
-        let directories = (await fs.readdir("repository", { withFileTypes: true })).filter(dirent => dirent.isDirectory() && !dirent.name.startsWith('.')).map(dirent => dirent.name);
+        let directories = (await fs.readdir(this.repository_path, { withFileTypes: true })).filter(dirent => dirent.isDirectory() && !dirent.name.startsWith('.')).map(dirent => dirent.name);
         for (let directory_index in directories) {
             let directory = directories[directory_index];
             try {
                 console.log("Processing app", directory);
-                let metadata = JSON.parse(await fs.readFile(Path.join("repository", directory, "metadata.json")));
+                let metadata = JSON.parse(await fs.readFile(Path.join(this.repository_path, directory, "metadata.json")));
                 for (let category_index in metadata.categories) {
                     let category = metadata.categories[category_index];
                     let category_slug = category.toLowerCase().replace(" ", "_");
@@ -113,18 +118,83 @@ class Repository {
         }
     }
 
+    async create_index_per_device() {
+        for (let project_index in this.projects) {
+            let project_wrapper = this.projects[project_index];
+            let project = project_wrapper.project;
+            for (let application_index in project.application) {
+                let application = project.application[application_index];
+                for (let target_index in application.targets) {
+                    let target = application.targets[target_index];
+                    if (!(target in this.index.categories_per_device)) {
+                        console.log("New device found:", target);
+                        this.index.categories_per_device[target] = {};
+                        this.projects_per_device[target] = {};
+                    }
+                    for (let category_index in project.categories) {
+                        let category = project.categories[category_index];
+                        let category_slug = category.toLowerCase().replace(" ", "_");
+                        if (!(category_slug in this.index.categories_per_device[target])) {
+                            console.log("Adding category", category_slug, "to device", target);
+                            this.index.categories_per_device[target][category_slug] = {
+                                name: this.index.categories[category_slug].name,
+                                projects: []
+                            };
+                        }
+                        if (!(project_index in this.index.categories[category_slug].projects)) {
+                            console.log("Adding project", project_index, "to category", category_slug, "for device", target);
+                            this.index.categories_per_device[target][category_slug].projects.push(project_index);
+
+                        }
+                    }
+                    if (!(project in this.projects_per_device[target])) {
+                        console.log("Adding project", project_index, "to device", target);
+                        this.projects_per_device[target][project_index] = project_wrapper;
+                    }
+                }
+            }
+        }
+    }
+
     async get_categories(device = null) {
-        return Object.keys(this.index.categories);
+        console.log("Request for get categories", device);
+        if (device === null) {
+            return Object.keys(this.index.categories);
+        } else {
+            if (device in this.index.categories_per_device) {
+                return Object.keys(this.index.categories_per_device[device]);
+            } else {
+                return [];
+            }
+        }
     }
 
     async get_category(category = null, device = null) {
-        if (category === null) {
+        if (device == null) {
+            if (category === null) {
+                return {
+                    name: 'All projects',
+                    projects: Object.keys(this.projects),
+                };
+            } else if (category in this.index.categories) {
+                return this.index.categories[category];
+            }
+        } else if (device in this.index.categories_per_device){
+            console.log("All projects for", device);
+            if (category === null) {
+                return {
+                    name: 'All projects',
+                    projects: Object.keys(this.projects_per_device[device]),
+                };
+            } else if (category in this.index.categories) {
+                console.log("Categories for", device, category, this.index.categories_per_device[device], this.index.categories_per_device[device][category]);
+                return this.index.categories_per_device[device][category];
+            }
+        } else {
             return {
-                name: 'All projects',
-                projects: Object.keys(this.projects),
+                name: category !== null  ? category : 'All projects',
+                projects: [],
             };
-        } else if (category in this.index.categories) {
-            return this.index.categories[category];
         }
         return null;
     }
@@ -141,7 +211,7 @@ app.get('/projects', async (req, res, next) => {
     let amount = ("amount" in req.query) ? Number(req.query.amount) : null;
     if (amount === NaN) amount = 0;
     let category_name = (typeof req.query.category === 'string') ? req.query.category : null;
-    let device_name = (typeof req.query.category === 'string') ? req.query.device : null;
+    let device_name = (typeof req.query.device === 'string') ? req.query.device : null;
 
     // Function
     let category = await repository.get_category(category_name, device_name);
@@ -185,7 +255,7 @@ app.get('/projects/:slug', async (req, res, next) => {
 });
 
 app.get('/categories', async (req, res, next) => {
-    let device_name = (typeof req.query.category === 'string') ? req.query.device : null;
+    let device_name = (typeof req.query.device === 'string') ? req.query.device : null;
     let categories = await repository.get_categories(device_name);
     res.json(categories);
 });
